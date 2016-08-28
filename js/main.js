7b93639c5b92392aa325287cff9a45b95fbd3a1f
Mike Semko <msemko@gmail.com>
2016-08-27 20:27:38 -0400

    changed the way results are stored. Added avatar ID and moved submit button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 (function() {
   'use strict';
-  angular.module('app').controller('Main', ['$http', Main]);
+  angular.module('app').controller('Main', ['$http', '$scope', Main]);
 
-  function Main($http) {
+  function Main($http, $scope) {
     var vm = this;
     vm.isLoaded = false;
     vm.schema = [];
-    vm.results = ["25","31","-1", "-2", "28", "7"];
+    vm.avatarDefaults = ["25","31","-1", "-2", "28", "7"];
+    vm.results = [["25","31","-1", "-2", "28", "7"]];
     vm.imageUrls = {};
     vm.currentUser = "";
+    vm.currentAvatar = 0;
+    vm.avatarName = "";
 
     vm.getImageById = function(id) {
       return imageUrls[id];
@@ -19,7 +22,7 @@
         method: 'GET',
         //url: '/schema.json'
         // url: 'http://default-environment.ymuptkfrgv.us-west-2.elasticbeanstalk.com/getAdultMaleParts'
-        url: 'http://178.62.255.163:8080/FamilyStoryWebService_26AUG2016/getAdultMaleParts'
+        url: 'http://178.62.255.163:8080/FamilyStoryWebService/getAdultMaleParts'
       }).then(function success(data){
       	vm.schema = data.data;
         vm.schema.forEach(function(item){
@@ -41,8 +44,8 @@
     vm.postPerson = function() {
       var response = {
         user_id: vm.currentUser,
-        avatar_name: "test_avatar1",
-        image_id_list: vm.results.map(function(item){return parseInt(item);})
+        avatar_name: vm.avatarName,
+        image_id_list: vm.results[vm.currentAvatar].map(function(item){return parseInt(item);})
       }
       // vm.schema.forEach(function(item){
       //   response.image_id_list += $('input[name=' + item.image_type + ']:checked').val() + ",";
@@ -52,12 +55,17 @@
       $http({
         method: 'POST',
         // url: 'http://default-environment.ymuptkfrgv.us-west-2.elasticbeanstalk.com/setUserSelection',
-        url: 'http://178.62.255.163:8080/FamilyStoryWebService_26AUG2016/setUserSelection',
+        url: 'http://178.62.255.163:8080/FamilyStoryWebService/setUserSelection',
         headers: {
           "Content-Type": "application/json"
         },
         data: response
       }).then(function(data){
+        if (data.data.result = "SUCCESS") {
+          alert("Saved!");
+          vm.results[0] = Array.prototype.slice.call(vm.avatarDefaults);
+          vm.avatarName = "";
+        }
         console.log(data);
       });
     }
